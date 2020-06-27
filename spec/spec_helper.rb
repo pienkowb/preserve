4fc8b2ab00da110065034141587148026c34d464
Bartosz Pieńkowski <pienkowb@gmail.com>
2020-06-27 21:52:57 +0200

    Use __dir__ instead of __FILE__

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,12 @@
 ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path('../dummy/config/environment.rb', __FILE__)
+require File.expand_path('dummy/config/environment.rb', __dir__)
 
 require 'rspec/rails'
 
 Rails.backtrace_cleaner.remove_silencers!
 
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+Dir["#{__dir__}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.color = true
