884bea6b5c6aa932510641f9c497d1e673d58b92
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2025-07-20 20:00:46 +0200

    Ensure session stored object are native types (#13)
    * rename HASH_CLASS to PARAMETERS_CLASS
    
    * ensure session stored objects are always native types (casting eventual PARAMETERS_CLASS to hash)

--- lib/preserve/callback.rb
+++ lib/preserve/callback.rb
@@ -49,14 +49,16 @@ module Preserve
 
     def parameter_value
       keys = Array(parameter_key)
-      keys.reduce(params) { |h, k| h[k] if h.is_a?(HASH_CLASS) }
+      value = keys.reduce(params) { |h, k| h[k] if h.is_a?(PARAMETERS_CLASS) }
+      value = parameters_to_hash(value) if value.is_a?(PARAMETERS_CLASS)
+      value
     end
 
     def parameter_value=(value)
       *keys, last_key = parameter_key
 
       nested_hash = keys.reduce(params) do |hash, key|
-        hash[key] ||= HASH_CLASS.new
+        hash[key] ||= PARAMETERS_CLASS.new
       end
 
       nested_hash[last_key] = value
@@ -64,6 +66,11 @@ module Preserve
 
     def session_key
       SessionKey.new(source_class, parameter_key).build
+    end
+
+    def parameters_to_hash(params)
+      params = params.to_unsafe_h if params.respond_to?(:to_unsafe_h)
+      params.to_hash
     end
   end
 end
